7a6ed5ab44ff74b4bc0a92058c24f9cedc6cb0f4
Md Shoaib Islam <mestabd60@gmail.com>
2022-03-12 04:08:17 +0600

    Aded identifire

---
+++ src/com/example/helloworld/identifire/identifire.java
@@ -0,0 +1,9 @@
+package com.example.helloworld.identifire;
+
+public class identifire {
+    public static void main(String[] args) {
+        int radius = 2;
+        double area = radius * radius * 3.1416;
+        System.out.println(area);
+    }
+}
