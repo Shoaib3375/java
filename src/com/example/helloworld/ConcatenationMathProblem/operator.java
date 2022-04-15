372a9ce4551ea5f16bb3c149b2a92dbf877efab9
Md Shoaib Islam <mestabd60@gmail.com>
2022-04-16 04:59:16 +0600

    Aded Operator for relational Operator

---
+++ src/com/example/helloworld/ConcatenationMathProblem/operator.java
@@ -0,0 +1,23 @@
+package com.example.helloworld.ConcatenationMathProblem;
+
+import java.util.Scanner;
+
+public class operator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+// Int this programm. we know about relational operator.
+        System.out.println("Enter a number: ");
+        int a = input.nextInt();
+        System.out.println("Enter another number: ");
+        int b = input.nextInt();
+        boolean isAGreaterThanB = a>b;
+        if (isAGreaterThanB){
+            System.out.println(a + " is greater than " + b);
+        }
+        boolean isALessThanB = a<b;
+        if(isALessThanB){
+            System.out.println(a + " is less than "+ b);
+        }
+
+    }
+}
