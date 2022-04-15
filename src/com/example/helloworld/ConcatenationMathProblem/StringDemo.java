c18d29a8c76e1b8daeac47273412d1e58ecb4cff
Md Shoaib Islam <mestabd60@gmail.com>
2022-04-16 04:40:26 +0600

    Aded Concatinating

---
+++ src/com/example/helloworld/ConcatenationMathProblem/StringDemo.java
@@ -0,0 +1,10 @@
+package com.example.helloworld.ConcatenationMathProblem;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String NationalAnthemLineOne = "My Bengal of Precious Gold, I love you. ";
+        String NationalAnthemLineTwo = "Forever your skies, your air set my heart in tune\n" + "As if it were a flute.";
+        String nationalAnthem = NationalAnthemLineOne + NationalAnthemLineTwo;
+        System.out.println(nationalAnthem);
+    }
+}
